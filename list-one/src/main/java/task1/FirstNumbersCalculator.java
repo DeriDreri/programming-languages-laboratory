dd9c36dd078e9fdca5b585a2008a224f9cc81bf5
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-27 22:49:58 +0100

    Corrects small syntax issues

--- list-one/src/main/java/task1/FirstNumbersCalculator.java
+++ list-one/src/main/java/task1/FirstNumbersCalculator.java
@@ -7,54 +7,56 @@ public class FirstNumbersCalculator {
 	private int foundPrimeNumbers;
 
 	public FirstNumbersCalculator(String number) {
-		
-		try{
+		try {
 			foundPrimeNumbers = findNumberOfPrimeNumbers(Integer.parseInt(number));
 		} catch (NumberFormatException e) {
 			foundPrimeNumbers = -1;
 		}
 	}
-	
+
 	public int getNumberOfFoundPrimeNumbers() {
 		return foundPrimeNumbers;
 	}
-	
-	private int findNumberOfPrimeNumbers(int number) throws NumberFormatException{
-		if (number <= 3) throw new NumberFormatException();
+
+	private int findNumberOfPrimeNumbers(int number) throws NumberFormatException {
+		if (number <= 3)
+			throw new NumberFormatException();
 		int sumOfPrimeNumbers = -1;
-		for(int i = 3; i <= number; i++) {
-			if(i < 15) {
-				int factorial = integerFactorial(i-2);
+		for (int i = 3; i <= number; i++) {
+			if (i < 15) {
+				int factorial = integerFactorial(i - 2);
 				sumOfPrimeNumbers += factorial - i * (factorial / i);
 				continue;
 			}
-			if(i < 23) {
-				long factorial = longFactorial(i-2);
+			if (i < 23) {
+				long factorial = longFactorial(i - 2);
 				sumOfPrimeNumbers += factorial - i * (factorial / i);
 				continue;
 			}
-			var factorial = BigIntegerFactorial(i-2);
-			sumOfPrimeNumbers += factorial.subtract(new BigInteger(Integer.toString(i)).multiply(factorial.divide(new BigInteger(Integer.toString(i))))).intValue();
+			var factorial = BigIntegerFactorial(i - 2);
+			sumOfPrimeNumbers += factorial.subtract(
+					new BigInteger(Integer.toString(i)).multiply(factorial.divide(new BigInteger(Integer.toString(i)))))
+					.intValue();
 		}
 		return sumOfPrimeNumbers;
 	}
 
 	private int integerFactorial(int number) {
 		int toReturn = 1;
-		for(int i = 2; i <= number && i < 13; i++) {
+		for (int i = 2; i <= number && i < 13; i++) {
 			toReturn *= i;
 		}
 		return toReturn;
 	}
-	
+
 	private long longFactorial(int number) {
 		long toReturn = integerFactorial(number);
-		for(int i = 13; i <= number && i < 21; i++) {
+		for (int i = 13; i <= number && i < 21; i++) {
 			toReturn *= i;
 		}
 		return toReturn;
 	}
-	
+
 	private BigInteger BigIntegerFactorial(int number) {
 		var toReturn = new BigInteger(Long.toString(longFactorial(number)));
 		for (int i = 21; i <= number; i++) {
